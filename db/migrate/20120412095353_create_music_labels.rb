d69d0d62ef5e5f31b100eedcad384b269f5f706b
Saaman <metalrom2000@yahoo.fr>
2012-05-01 23:46:10 +0200

    Configuring local_prod to run on local PG instance

--- db/migrate/20120412095353_create_music_labels.rb
+++ db/migrate/20120412095353_create_music_labels.rb
@@ -1,5 +1,5 @@
 class CreateMusicLabels < ActiveRecord::Migration
-  def change
+  def up
     create_table :music_labels do |t|
       t.string :name
       t.string :website
@@ -9,7 +9,15 @@ class CreateMusicLabels < ActiveRecord::Migration
     end
 
     add_column :albums, :music_label_id, :integer
-    #add_foreign_key(:albums, :music_labels)
+    add_foreign_key :albums, :music_labels
     add_index :music_labels, :name, :unique => true
+  end
+
+  def down
+    remove_index :music_labels, :name
+    remove_foreign_key :albums, :music_labels
+    remove_column :albums, :music_label_id
+
+    drop_table :music_labels
   end
 end
