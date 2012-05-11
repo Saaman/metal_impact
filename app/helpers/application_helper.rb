b94b6bf56cbb6f4fb4948d1db108d8fa6b500fff
Metalrom <metalrom2000@yahoo.fr>
2012-05-11 18:22:54 +0200

    turn registration form as modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
 	def get_back_uri
 		session[:back_uri] || root_path
 	end
+
+	#this method should be used instead of 'yield' when using content_for with partial layouts
+	def yield_content!(content_key)
+    view_flow.content.delete(content_key)
+  end
 end
