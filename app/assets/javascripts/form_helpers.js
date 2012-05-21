0f582041b396f83a6ff033d7742938c90472ba48
Saaman <metalrom2000@yahoo.fr>
2012-05-22 00:33:06 +0200

    validations OK

--- app/assets/javascripts/form_helpers.js
+++ app/assets/javascripts/form_helpers.js
@@ -1,10 +1,14 @@
 jQuery.fn.bindToCleanErrorStyles = function() {
-	$(this).on('focus', '.control-group.error input', function(event) {
+	$(this).on('focus', '.control-group.error input, .control-group.success input', function(event) {
   //when focusing an input, remove any error formatting + inline help
-		$(this).parents('.control-group').removeClass('error').end().siblings('span.help-inline').remove();
+		$(this).parents('.control-group').removeClass('error').removeClass('success').end().siblings('span.help-inline').remove();
 	})
 }
 
 jQuery.fn.displayDynamicError = function(message) {
 	$(this).after('<span class="help-inline">' + message + '</span>').parents('.control-group').addClass('error');
+}
+
+jQuery.fn.displayDynamicValidation = function() {
+	$(this).parents('.control-group').addClass('success');
 }
