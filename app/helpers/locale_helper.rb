3ba7b1e55c73678dcbf9bb31de0e1165f9eb1d25
Metalrom <metalrom2000@yahoo.fr>
2012-06-20 17:35:01 +0200

    login page look and feel

--- app/helpers/locale_helper.rb
+++ app/helpers/locale_helper.rb
@@ -4,6 +4,16 @@ module LocaleHelper
 		I18n.locale = retrieve_from_user_profile || retrieve_from_headers || I18n.default_locale
 	end
 
+	def localized_image_tag(image_name, options={})
+		localized_image_name = String.new(image_name)
+		localized_image_name.insert(image_name.rindex('.'), "-#{I18n.locale}")
+		if File.exist?(image_path(localized_image_name))
+			image_tag(localized_image_name, options)
+		else
+			image_tag(image_name, options)
+		end
+	end
+
 	private
 		def retrieve_from_headers
 			request.preferred_language_from(I18n.available_locales) || request.compatible_language_from(I18n.available_locales)
@@ -13,4 +23,5 @@ module LocaleHelper
 			parsed_locale = params[:locale]
 			I18n.available_locales.include?(parsed_locale) ? parsed_locale  : nil
 		end
+
 end
