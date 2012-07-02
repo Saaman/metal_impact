7bf1afe3d6a6992688b62ceefcce57b9594aa811
Saaman <metalrom2000@yahoo.fr>
2012-07-02 23:54:04 +0200

    set PG as DB server, review new password page

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,5 +1,24 @@
 class Users::PasswordsController < Devise::PasswordsController
 
+def create
+	authorize! :create, User
+  build_resource
+
+  logger.info "email : #{resource.email}"
+
+  if verify_recaptcha(:model => resource)
+  	unless User.find_by_email(params[:user][:email]).nil?
+    	super
+    	return
+    else
+    	resource.errors[:base] << I18n.t("devise.passwords.errors.email_taken", :email => params[:user][:email])
+    end
+  end
+  respond_with resource do |format|
+    format.html  { render 'new' }
+  end
+end
+
 protected
 
     def after_sending_reset_password_instructions_path_for(resource)
