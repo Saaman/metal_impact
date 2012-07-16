b3eb7fa294e15a1bce6dab3c91e3878b016fc77e
Saaman <metalrom2000@yahoo.fr>
2012-07-17 00:20:12 +0200

    finish language management (except on user profile)

--- app/helpers/locale_helper.rb
+++ app/helpers/locale_helper.rb
@@ -1,13 +1,15 @@
 module LocaleHelper
 
 	def set_locale
-		I18n.locale = params[:locale] || retrieve_from_user_profile || retrieve_from_headers || I18n.default_locale
+		I18n.locale = retrieve_from_url || retrieve_from_headers || I18n.default_locale
 	end
 
-	def localized_image_tag(image_name, options={})
+	def localized_image_tag(image_name, locale_param=I18n.locale, options={})
 		localized_image_name = String.new(image_name)
-		localized_image_name.insert(image_name.rindex('.'), "-#{I18n.locale}")
-		if File.exist?(image_path(localized_image_name))
+		localized_image_name.insert(image_name.rindex('.'), "-#{locale_param}")
+		logger.info "localized_image_name = #{path_to_image(localized_image_name)}"
+		unless MetalImpact::Application.assets.find_asset(localized_image_name).nil?
+			logger.info("asset trouve : #{MetalImpact::Application.assets.find_asset(localized_image_name).inspect()}")
 			image_tag(localized_image_name, options)
 		else
 			image_tag(image_name, options)
@@ -16,12 +18,13 @@ module LocaleHelper
 
 	private
 		def retrieve_from_headers
-			request.preferred_language_from(I18n.available_locales) || request.compatible_language_from(I18n.available_locales)
+			request.preferred_language_from(I18n.available_locales) || request.language_region_compatible_from(I18n.available_locales)
 		end
 
-		def retrieve_from_user_profile
-			parsed_locale = params[:locale]
-			I18n.available_locales.include?(parsed_locale) ? parsed_locale  : nil
+		def retrieve_from_url
+			parsed_locale = params[:locale].to_sym
+			res = I18n.available_locales.include?(parsed_locale) ? parsed_locale  : nil
+			return res
 		end
 
 end
