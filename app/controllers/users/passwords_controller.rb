bd1d220748fd959987d71bc296fdd120fa8dc6a9
Metalrom <metalrom2000@yahoo.fr>
2012-07-23 17:11:15 +0200

    users views minor fixes

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -2,16 +2,16 @@ class Users::PasswordsController < Devise::PasswordsController
 
 def create
   build_resource
-
-  @email = resource.email
+  @user = resource
   if verify_recaptcha(:model => resource)
-  	unless User.find_by_email(@email).nil?
+  	unless User.find_by_email(resource.email).nil?
     	super
-    	return
+      return
     else
     	resource.errors[:base] << I18n.t("devise.passwords.errors.email_taken", :email => params[:user][:email])
     end
   end
+
   respond_with resource do |format|
     format.html  { render 'new' }
   end
@@ -19,13 +19,13 @@ end
 
 def email_sent
   flash.clear  #prevent devise from displaying the email sending message as flash notice
-  @email = params[:email]
+  @user = User.find(params[:id])
 end
 
 protected
 
     def after_sending_reset_password_instructions_path_for(resource_name)
-      logger.info("email = #{resource.email}")
-    	email_sent_user_password_path(email: @email)
+      #logger.info("@user = #{@user}")
+    	email_sent_user_password_path(id: @user)
     end
 end
