adfb220af0af4031f3b5664718379963b73a9389
Saaman <metalrom2000@yahoo.fr>
2012-09-14 00:14:11 +0200

    delete unused rspec support files

--- spec/support/helper_macros.rb
+++
@@ -1,9 +0,0 @@
-module HelperMacros
-  def stub_abilities
-    before do
-      @ability = Object.new
-      @ability.extend(CanCan::Ability)
-      helper.stub(:current_ability).and_return(@ability)
-    end
-  end
-end
