efbc3de560a272a1b397bee8a3f0681206fe0770
Metalrom <metalrom2000@yahoo.fr>
2012-10-05 18:08:36 +0200

    switch carrierwave storage to google storage

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -7,17 +7,17 @@ class CoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}_#{mounted_as}"
+    "#{model.class.to_s.underscore}_#{mounted_as}s"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,4 +6,11 @@ CarrierWave.configure do |config|
 
 	config.permissions = 0664
   #config.directory_permissions = 0771
+
+  config.fog_credentials = {
+    :provider                         => 'Google',
+    :google_storage_access_key_id     => 'GOOGK2RDVAHAQFSPV7XU',
+    :google_storage_secret_access_key => 'XaD3Mvb6HCXVe+qAPljIQJdlIpt3pgatQLAW1Irk'
+  }
+  config.fog_directory = 'metalimpact'
 end
