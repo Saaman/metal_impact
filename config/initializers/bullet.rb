a3e943805b1130dd31a52da2bf1ab87bad83d0b9
Saaman <metalrom2000@yahoo.fr>
2012-10-07 22:08:54 +0200

    adds bullet gem

---
+++ config/initializers/bullet.rb
@@ -0,0 +1,7 @@
+#smart way to configure the developement tool. It will be configured only if gem is present, i.e. in development (as part of gemfile group development)
+if defined? Bullet
+  Bullet.enable = true
+  Bullet.bullet_logger = true
+  Bullet.console = true
+end
+
