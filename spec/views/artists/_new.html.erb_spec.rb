48322ef1d0b33e8d79d79ed02b552f11adba19a0
Metalrom <metalrom2000@yahoo.fr>
2012-10-17 13:31:42 +0200

    finish first impl of artist creation modal form

--- spec/views/artists/_new.html.erb_spec.rb
+++ spec/views/artists/_new.html.erb_spec.rb
@@ -8,12 +8,14 @@ describe "artists/_new" do
 
   it "renders artist _new partial" do
     stub_template "shared/_error_messages.html.erb" => "<div>shared_error_messages.html.erb</div>"
-    render :partial => "artists/_new"
+    render :partial => "artists/new"
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should render_template(:partial => "shared/_error_messages", :locals => { :object => artist } )
     rendered.should have_selector "form", :action => artists_path do |form|
       form.should have_selector "input", id: "artist_name", type: "text"
       form.should have_selector "select", id: "artist_countries"
+      form.should have_selector "textarea", id: "artist_biography"
       form.should have_selector "input", type: "submit"
     end
   end
